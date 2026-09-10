bc97f3ae3f1ebef7a9aacee6767af59e79134d60
nicky4gun <nicklas.stagesorensen@outlook.dk>
2026-09-10 13:39:11 +0200

    Add simple Client - Manuel test connects to server

--- src/main/java/org/example/ChatClient.java
+++ src/main/java/org/example/ChatClient.java
@@ -1,4 +1,27 @@
 package org.example;
 
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class ChatClient {
+    private static final String DEFAULT_HOST = "localhost";
+    private static final int DEFAULT_PORT = 5001;
+
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String message = scanner.nextLine();
+
+        try (Socket socket = new Socket(DEFAULT_HOST, DEFAULT_PORT);
+             PrintWriter out = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()), true)) {
+
+            out.println(message);
+            System.out.println("Client connected and sent: " + message);
+        } catch (Exception e) {
+            System.out.println("Client error: " + e.getMessage());
+        }
+    }
 }
